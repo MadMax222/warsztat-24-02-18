296805d941460db0aa922b13f4aae7408246b424
Rafał Bedynek <rafal.bedynek@gmail.com>
2018-02-24 12:13:52 +0100

    Data types - primitives

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('Hello Rafał!')
+
+// data types in JS
+
+//string
+var textVariable = "Rafał"
+console.log(textVariable)
+
+// number
+var numberVariable = 43
+console.log(numberVariable)
+
+// boolean
+var boolVariable = true
+console.log(boolVariable)
+
+// null
+var nullVariable = null
+console.log(nullVariable)
+
+// undefined
+var undefinedVariable = undefined
+console.log(undefinedVariable)
