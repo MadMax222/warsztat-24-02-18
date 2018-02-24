08c52c8751eb947a4c92f1856848bb0626215a96
Rafał Bedynek <rafal.bedynek@gmail.com>
2018-02-24 14:01:00 +0100

    Function intervals

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+(function (){
+    var secondsCounter = 1
 
-var secondsCounter = 1
+    var intervalId = setInterval(
+        function(){
+            console.log(secondsCounter++)
+        },
+        1000
+    )
 
-var intervalId = setInterval(
-    function(){
-        console.log(secondsCounter++)
-    },
-    1000
-)
-
-setTimeout(
-    function(){
-        clearInterval(intervalId)
-    },
-    5000
-)
+    setTimeout(
+        function(){
+            clearInterval(intervalId)
+        },
+        5000
+    )
+})()
