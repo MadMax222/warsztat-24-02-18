ddab3b1624af81f3ad42f4c0ff65339ff536736e
Rafał Bedynek <rafal.bedynek@gmail.com>
2018-02-24 12:50:34 +0100

    object

--- script.js
+++ script.js
@@ -23,3 +23,37 @@ console.log(nullVariable)
 // undefined
 var undefinedVariable = undefined
 console.log(undefinedVariable)
+
+// objects with data
+
+var simplestObj = {
+    name: 'Rafał',
+    lastname: 'Bedynek'
+}
+console.log(simplestObj)
+
+// nested objects with data
+
+var nestedObj = {
+    name: 'Rafał',
+    car: {
+        brand: 'Renaul',
+        model: 407
+    }
+}
+console.log(nestedObj)
+console.log(nestedObj.car.model)
+console.log(nestedObj.car.year)
+
+// arrays
+var simpleArray = [
+    1,
+    {name: 'Ala', array:[1,4,6]},
+    3,
+    4,
+    5,
+]
+console.log(simpleArray)
+console.log(simpleArray[4])
+console.log(simpleArray[1].name)
+console.log(simpleArray[1].['name'])
