318ce49a85d02161a2703ad4b5aa6ab31dc80f87
Rafał Bedynek <rafal.bedynek@gmail.com>
2018-02-24 13:17:03 +0100

    multiple arguments

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
 // function declaration
-function hello(name){ // paramet is a name 
+function hello(name, y){ // paramet is a name 
+    
+    for(var i = 0; i < y; i++) {
     console.log('Hello ' + name + '!')
-    console.log('Hello ' + name + '!')
-    console.log('Hello ' + name + '!')
+    }
 }
 
 // function invocation (call)
-hello('Rafał') // passing atribute 'Rafał'
-hello('Ala')
-hello('Ela')
+hello('Rafał', 1) // passing atribute 'Rafał'
+hello('Ala', 2)
+hello('Ela', 3)
+hello({name: 'Stanisława'}, 1)
