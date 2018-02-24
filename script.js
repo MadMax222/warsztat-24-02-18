d8b6a633ecb10ed75ba0dec2af5a87e61b3b8891
Rafał Bedynek <rafal.bedynek@gmail.com>
2018-02-24 15:36:42 +0100

    Creating mole

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 (function(){
-    
-})
+
+    function makeMole(){
+        var molePosX = Math.round(
+            Math.random() * (window.innerWidth - window.innerHeight/10)
+        )
+        var molePosY = Math.round(
+            Math.random() * (window.innerHeight - window.innerHeight/10)
+        )
+
+        var mole = document.createElement('div')
+
+        mole.classList.add('mole')
+
+        console.log(mole.style)
+
+        mole.style.left = molePosX + 'px'
+        mole.style.top = molePosY + 'px'
+
+        document.querySelector('body').appendChild(mole)
+    }
+
+    makeMole()
+})()
