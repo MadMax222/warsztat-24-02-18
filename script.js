5c5621516094f10be78c002c50dce5979205b54c
Rafał Bedynek <rafal.bedynek@gmail.com>
2018-02-24 13:42:49 +0100

    Intervals and timeouts

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
 
-setInterval(
+var secondsCounter = 1
+
+var intervalId = setInterval(
     function(){
-        console.log('Minęła sekunda')
+        console.log(secondsCounter++)
     },
     1000
 )
 
-var globalVariable = 'GLOBAL'
-
-// function declaration
-function add(a, b) {
-    var someVariable = 123
-    console.log(globalVariable)
-    return a + b
-}
-
-// function expression
-var add2 = function add(a, b) {
-    var someVariable = 123
-    console.log(globalVariable)
-    return a + b
-}
-
-//assing function to nother variable
-var add3 = add2
-
-var result = add3(3, 5)
-
-console.log(result)
+setTimeout(
+    function(){
+        clearInterval(intervalId)
+    },
+    5000
+)
